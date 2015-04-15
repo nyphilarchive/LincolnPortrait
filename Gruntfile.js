53f9cad7e5f47c212a5cc577d9febe37dd340a3c
mjbrodsky <mjbrodsky@gmail.com>
2015-04-14 21:08:39 -0400

    Levy logo and creds in STICKY footer

--- Gruntfile.js
+++ Gruntfile.js
@@ -335,6 +335,12 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= config.dist %>'
+        }, {
+          expand: true,
+          dot: true,
+          cwd: '.',
+          src: 'bower_components/components-font-awesome/fonts/*',
+          dest: '<%= config.dist %>'
         }]
       },
       styles: {
