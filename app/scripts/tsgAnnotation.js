546ae7ebcfae17dcf3b4cf15d5eb88eff08402f8
dan <dlafeir@umich.edu>
2015-04-15 12:27:51 -0500

    adding fullscreen

--- app/scripts/tsgAnnotation.js
+++ app/scripts/tsgAnnotation.js
@@ -20,7 +20,8 @@ var OpenDANnotate = function(){
     rightId: "annotated-image-right",
     rightImageOffset: 25,
     canvasWidth : 855,
-    canvasHeight: 580
+    canvasHeight: 580,
+    movementScale: .99
   };
 
   TSG.Images.Large = {
@@ -32,7 +33,8 @@ var OpenDANnotate = function(){
     rightId: "annotated-image-right-lg",
     rightImageOffset: 25,
     canvasWidth : 855,
-    canvasHeight: 600
+    canvasHeight: 600,
+    movementScale: .99
   };
 
   TSG.Images.XLarge = {
@@ -111,9 +113,20 @@ var OpenDANnotate = function(){
     };
   };
 
+  TSG.Utils.mobileCheck = function() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  }
+
   TSG.Utils.drawImage = function(ctx){
-    ctx.drawImage(TSG.Canvas.annotatedImageLeft,0,0);
-    ctx.drawImage(TSG.Canvas.annotatedImageRight,(TSG.Images.CurrentImage.canvasWidth/TSG.Images.CurrentImage.origScale.width)/2-TSG.Images.CurrentImage.rightImageOffset,0);
+    if(TSG.Utils.mobileCheck() === true){
+      ctx.drawImage(TSG.Canvas.annotatedImageRight,0,0);
+    }
+    else {
+      ctx.drawImage(TSG.Canvas.annotatedImageLeft,0,0);
+      ctx.drawImage(TSG.Canvas.annotatedImageRight,(TSG.Images.CurrentImage.canvasWidth/TSG.Images.CurrentImage.origScale.width)/2-TSG.Images.CurrentImage.rightImageOffset,0);
+    }
   }
 
   TSG.Utils.drawCircle = function(ctx){
@@ -136,11 +149,11 @@ var OpenDANnotate = function(){
     //   TSG.Images.CurrentImage = TSG.Images.XLarge;
     // }
     // else
-    if(TSG.Canvas.annotations.scale === 1.5){
-      TSG.Canvas.annotatedImageLeft = document.getElementById('annotated-image-left-lg');
-      TSG.Canvas.annotatedImageRight = document.getElementById('annotated-image-right-lg');
-      TSG.Images.CurrentImage = TSG.Images.Large;
-    }
+    // if(TSG.Canvas.annotations.scale){
+    //   TSG.Canvas.annotatedImageLeft = document.getElementById('annotated-image-left-lg');
+    //   TSG.Canvas.annotatedImageRight = document.getElementById('annotated-image-right-lg');
+    //   TSG.Images.CurrentImage = TSG.Images.Large;
+    // }
 
     //use recursion to ease the zoom;
     var pt = TSG.Canvas.ctx.transformedPoint(TSG.Images.CurrentImage.canvasWidth/2,TSG.Images.CurrentImage.canvasHeight/2);
@@ -161,20 +174,39 @@ var OpenDANnotate = function(){
   }
 
   TSG.Utils.reset = function reset() {
-    TSG.Images.CurrentImage = TSG.Images.Regular;
-    TSG.Canvas.canvas.width = TSG.Images.Regular.canvasWidth;
-    TSG.Canvas.canvas.height = TSG.Images.Regular.canvasHeight;
 
-    TSG.Canvas.ctx = TSG.Canvas.canvas.getContext('2d');
-    TSG.Utils.trackTransforms(TSG.Canvas.ctx);
-    TSG.Canvas.ctx.scale(TSG.Images.Regular.origScale.width,TSG.Images.Regular.origScale.height); //set the starting scale
-    TSG.Canvas.ctx.clearRect (0,0,TSG.Canvas.canvas.width,TSG.Canvas.canvas.height);
+    if(TSG.Utils.mobileCheck() === true){
+      TSG.Images.CurrentImage = TSG.Images.Regular;
+      TSG.Canvas.canvas.width = TSG.Images.Regular.canvasWidth/2;
+      TSG.Canvas.canvas.height = TSG.Images.Regular.canvasHeight;
 
-    TSG.Utils.drawImage(TSG.Canvas.ctx);
+      TSG.Canvas.ctx = TSG.Canvas.canvas.getContext('2d');
+      TSG.Utils.trackTransforms(TSG.Canvas.ctx);
+      TSG.Canvas.ctx.scale(TSG.Images.Regular.origScale.width,TSG.Images.Regular.origScale.height); //set the starting scale
+      TSG.Canvas.ctx.clearRect (0,0,TSG.Canvas.canvas.width,TSG.Canvas.canvas.height);
 
-    TSG.Canvas.annotations.offsetX = 0;
-    TSG.Canvas.annotations.offsetY = 0;
-    TSG.Canvas.annotations.scale = 1;
+      TSG.Utils.drawImage(TSG.Canvas.ctx);
+
+      TSG.Canvas.annotations.offsetX = 0;
+      TSG.Canvas.annotations.offsetY = 0;
+      TSG.Canvas.annotations.scale = 1;
+    }
+    else {
+      TSG.Images.CurrentImage = TSG.Images.Regular;
+      TSG.Canvas.canvas.width = TSG.Images.Regular.canvasWidth;
+      TSG.Canvas.canvas.height = TSG.Images.Regular.canvasHeight;
+
+      TSG.Canvas.ctx = TSG.Canvas.canvas.getContext('2d');
+      TSG.Utils.trackTransforms(TSG.Canvas.ctx);
+      TSG.Canvas.ctx.scale(TSG.Images.Regular.origScale.width,TSG.Images.Regular.origScale.height); //set the starting scale
+      TSG.Canvas.ctx.clearRect (0,0,TSG.Canvas.canvas.width,TSG.Canvas.canvas.height);
+
+      TSG.Utils.drawImage(TSG.Canvas.ctx);
+
+      TSG.Canvas.annotations.offsetX = 0;
+      TSG.Canvas.annotations.offsetY = 0;
+      TSG.Canvas.annotations.scale = 1;
+    }
   }
 
   TSG.Utils.showMousePosition = function(){
@@ -208,30 +240,42 @@ var OpenDANnotate = function(){
       TSG.Utils.zoom(1/1.5);
       TSG.Canvas.annotations.offsetX *= 1/1.5
     });
+    TSG.Buttons.FullScreen = $('#fullscreen-btn').on('click', function(){
+      $('#image-title').toggle();
+      $('#annotation-facts').toggle();
+      $('#annotation-container').toggleClass('fullscreen-toggle', 'tsg-container');
+      $('#fullscreen-btn > span').toggleClass('glyphicon-resize-small','glyphicon-resize-full');
+      TSG.Utils.reset();
 
+    });
+
+    var mouseUpAndDown = false;
     //set up events
     TSG.Events.mousedown = $('#c').mousedown(function(evt){
       evt.preventDefault();
   	  //document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
-  		mouseX = evt.offsetX || (evt.pageX - TSG.Canvas.canvas.offsetLeft);
-      lastX = evt.offsetX || (evt.pageX - TSG.Canvas.canvas.offsetLeft);
-  		mouseY = evt.offsetY || (evt.pageY - TSG.Canvas.canvas.offsetTop);
-      lastY = evt.offsetY || (evt.pageY - TSG.Canvas.canvas.offsetTop);
+  		if(mouseUpAndDown === false){
+        mouseX = evt.offsetX || (evt.pageX - TSG.Canvas.canvas.offsetLeft);
+        lastX = evt.offsetX || (evt.pageX - TSG.Canvas.canvas.offsetLeft);
+    		mouseY = evt.offsetY || (evt.pageY - TSG.Canvas.canvas.offsetTop);
+        lastY = evt.offsetY || (evt.pageY - TSG.Canvas.canvas.offsetTop);
 
-    	dragStart = TSG.Canvas.ctx.transformedPoint(lastX,lastY);
-  		dragged = false;
+      	dragStart = TSG.Canvas.ctx.transformedPoint(lastX,lastY);
+    		dragged = false;
+      }
+      mouseUpAndDown = true;
   	});
     TSG.Events.mousemove = $('#c').mousemove(function(evt){
   		lastX = evt.offsetX || (evt.pageX - TSG.Canvas.canvas.offsetLeft);
   		lastY = evt.offsetY || (evt.pageY - TSG.Canvas.canvas.offsetTop);
   		dragged = true;
-  		if (dragStart){
-  			var pt = TSG.Canvas.ctx.transformedPoint(lastX,lastY);
-        TSG.Canvas.ctx.translate(pt.x-dragStart.x,pt.y-dragStart.y);
-        TSG.Utils.redraw();
-  		}
+  		// if (dragStart){
+  		// 	var pt = TSG.Canvas.ctx.transformedPoint(lastX,lastY);
+      //   TSG.Canvas.ctx.translate(pt.x-dragStart.x,pt.y-dragStart.y);
+      //   TSG.Utils.redraw();
+  		// }
   	});
-    TSG.Events.mousemove = $('#c').mouseup(function(evt){
+    TSG.Events.mouseup = $('#c').mouseup(function(evt){
       somelastX = evt.offsetX || (evt.pageX - TSG.Canvas.canvas.offsetLeft);
       somelastY = evt.offsetY || (evt.pageY - TSG.Canvas.canvas.offsetTop);
 
@@ -243,7 +287,7 @@ var OpenDANnotate = function(){
           TSG.Canvas.annotations.toggleAnnotation(index);
         }
       }
-
+      mouseUpAndDown = false;
       dragStart = false;;
   	});
 
@@ -254,8 +298,11 @@ var OpenDANnotate = function(){
           audio.pause();
           audio.currentTime = 0;
         });
+        if(TSG.Canvas.canvas === document.getElementById('c')){
+          TSG.Canvas.canvas = document.getElementById('c');
+        }
     });
-    TSG.Utils.showMousePosition();
+    //TSG.Utils.showMousePosition();
   };
 
   TSG.Canvas.canvas = document.getElementById('c');
@@ -381,15 +428,25 @@ var OpenDANnotate = function(){
         var scaledY1 = centerOfCanvas.y - (centerOfCanvas.y-annotation.y1)*self.scale;
         var scaledY2 = centerOfCanvas.y - (centerOfCanvas.y-annotation.y2)*self.scale;
 
-        var normalizedX1 = scaledX1*($('#c').width()/TSG.Canvas.OGCanvasSize.width);
-        var normalizedX2 = scaledX2*($('#c').width()/TSG.Canvas.OGCanvasSize.width);
-        var normalizedY1 = scaledY1*($('#c').height()/TSG.Canvas.OGCanvasSize.height);
-        var normalizedY2 = scaledY2*($('#c').height()/TSG.Canvas.OGCanvasSize.height);
+        if(TSG.Utils.mobileCheck() === true){
+          var normalizedX1 = scaledX1*(2*$('#c').width()/TSG.Canvas.OGCanvasSize.width);
+          var normalizedX2 = scaledX2*(2*$('#c').width()/TSG.Canvas.OGCanvasSize.width);
+          normalizedX1 -= $('#c').width();
+          normalizedX2 -= $('#c').width();
+          var normalizedY1 = scaledY1*($('#c').height()/TSG.Canvas.OGCanvasSize.height);
+          var normalizedY2 = scaledY2*($('#c').height()/TSG.Canvas.OGCanvasSize.height);
+        }
+        else {
+          var normalizedX1 = scaledX1*($('#c').width()/TSG.Canvas.OGCanvasSize.width);
+          var normalizedX2 = scaledX2*($('#c').width()/TSG.Canvas.OGCanvasSize.width);
+          var normalizedY1 = scaledY1*($('#c').height()/TSG.Canvas.OGCanvasSize.height);
+          var normalizedY2 = scaledY2*($('#c').height()/TSG.Canvas.OGCanvasSize.height);
+        }
 
-        var x1AxisWithOffset = (normalizedX1)+self.offsetX*1.1211;
-        var x2AxisWithOffset = (normalizedX2)+self.offsetX*1.1211;
-        var y1AxisWithOffset = (normalizedY1)+self.offsetY*1.1211;
-        var y2AxisWithOffset = (normalizedY2)+self.offsetY*1.1211;
+        var x1AxisWithOffset = (normalizedX1)+self.offsetX*TSG.Images.Regular.movementScale;
+        var x2AxisWithOffset = (normalizedX2)+self.offsetX*TSG.Images.Regular.movementScale;
+        var y1AxisWithOffset = (normalizedY1)+self.offsetY*TSG.Images.Regular.movementScale;
+        var y2AxisWithOffset = (normalizedY2)+self.offsetY*TSG.Images.Regular.movementScale;
 
         // if(index === 0){
         //   console.log("DEBUG annotations "+index);
